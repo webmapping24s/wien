8266a0da190db1339c2ff47fde49996fd8ae5c6b
Klaus Förster <klaus.foerster@uibk.ac.at>
2024-04-18 18:10:53 +0200

    alle thematischen Layer einschalten

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ let startLayer = L.tileLayer.provider("BasemapAT.grau");
 startLayer.addTo(map);
 
 let themaLayer = {
-  sights: L.featureGroup(),
-  lines: L.featureGroup(),
-  stops: L.featureGroup(),
-  zones: L.featureGroup(),
+  sights: L.featureGroup().addTo(map),
+  lines: L.featureGroup().addTo(map),
+  stops: L.featureGroup().addTo(map),
+  zones: L.featureGroup().addTo(map),
   hotels: L.featureGroup().addTo(map),
 }
 
@@ -124,7 +124,7 @@ async function loadHotels(url) {
   // console.log(geojson);
   L.geoJSON(geojson, {
     onEachFeature: function (feature, layer) {
-      console.log(feature);
+      //console.log(feature);
       layer.bindPopup(`
       `);
     }
