0af9532846bc9d963882ffcdbd0c4265d0cb6c9c
Klaus Förster <klaus.foerster@uibk.ac.at>
2024-04-18 15:44:43 +0200

    template für wien hinzugefügt

---
+++ main.js
@@ -0,0 +1,41 @@
+/* Vienna Sightseeing Beispiel */
+
+// Stephansdom Objekt
+let stephansdom = {
+  lat: 48.208493,
+  lng: 16.373118,
+  title: "Stephansdom",
+};
+
+// Karte initialisieren
+let map = L.map("map").setView([stephansdom.lat, stephansdom.lng], 12);
+
+// BasemapAT Layer mit Leaflet provider plugin als startLayer Variable
+let startLayer = L.tileLayer.provider("BasemapAT.grau");
+startLayer.addTo(map);
+
+// Hintergrundlayer
+L.control
+  .layers({
+    "BasemapAT Grau": startLayer,
+    "BasemapAT Standard": L.tileLayer.provider("BasemapAT.basemap"),
+    "BasemapAT High-DPI": L.tileLayer.provider("BasemapAT.highdpi"),
+    "BasemapAT Gelände": L.tileLayer.provider("BasemapAT.terrain"),
+    "BasemapAT Oberfläche": L.tileLayer.provider("BasemapAT.surface"),
+    "BasemapAT Orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
+    "BasemapAT Beschriftung": L.tileLayer.provider("BasemapAT.overlay"),
+  })
+  .addTo(map);
+
+// Marker Stephansdom
+L.marker([stephansdom.lat, stephansdom.lng])
+  .addTo(map)
+  .bindPopup(stephansdom.title)
+  .openPopup();
+
+// Maßstab
+L.control
+  .scale({
+    imperial: false,
+  })
+  .addTo(map);
