96c3c25cda1c52c7905820cb2e12132a0a9cf989
Klaus Förster <klaus.foerster@uibk.ac.at>
2024-04-25 18:35:06 +0200

    Alle Layer einschalten und auf Innere Stadt blicken

--- main.js
+++ main.js
@@ -8,17 +8,17 @@ let stephansdom = {
 };
 
 // Karte initialisieren
-let map = L.map("map").setView([stephansdom.lat, stephansdom.lng], 12);
+let map = L.map("map").setView([stephansdom.lat, stephansdom.lng], 15);
 
 // BasemapAT Layer mit Leaflet provider plugin als startLayer Variable
 let startLayer = L.tileLayer.provider("BasemapAT.grau");
 startLayer.addTo(map);
 
 let themaLayer = {
-  sights: L.featureGroup(),
-  lines: L.featureGroup(),
-  stops: L.featureGroup(),
-  zones: L.featureGroup(),
+  sights: L.featureGroup().addTo(map),
+  lines: L.featureGroup().addTo(map),
+  stops: L.featureGroup().addTo(map),
+  zones: L.featureGroup().addTo(map),
   hotels: L.markerClusterGroup({
     disableClusteringAtZoom: 17
   }).addTo(map),
