cbcad01575d27d9b02f1abd31beb79f3b9ba0025
Klaus Förster <klaus.foerster@uibk.ac.at>
2024-04-25 16:19:03 +0200

    Liniennetz eingefärbt nach Liniennamen

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ startLayer.addTo(map);
 
 let themaLayer = {
   sights: L.featureGroup(),
-  lines: L.featureGroup(),
+  lines: L.featureGroup().addTo(map),
   stops: L.featureGroup(),
-  zones: L.featureGroup().addTo(map),
+  zones: L.featureGroup(),
   hotels: L.featureGroup(),
 }
 
@@ -78,6 +78,30 @@ async function loadLines(url) {
   let geojson = await response.json();
   // console.log(geojson);
   L.geoJSON(geojson, {
+    style: function(feature) {
+      //console.log(feature.properties.LINE_NAME);
+      let lineName = feature.properties.LINE_NAME;
+      let lineColor = "black";
+      if (lineName == "Red Line") {
+        lineColor = "#FF4136";
+      } else if (lineName == "Yellow Line") {
+        lineColor = "#FFDC00";
+      } else if (lineName == "Blue Line") {
+        lineColor = "#0074D9";
+      } else if (lineName == "Green Line") {
+        lineColor = "#2ECC40";
+      } else if (lineName == "Grey Line") {
+        lineColor = "#AAAAAA";
+      } else if (lineName == "Orange Line") {
+        lineColor = "#FF851B";
+      } else {
+        // vielleicht kommen noch andere Linien dazu ...
+      }
+
+      return {
+        color: lineColor,
+      }
+    },
     onEachFeature: function (feature, layer) {
       //console.log(feature);
       layer.bindPopup(`
